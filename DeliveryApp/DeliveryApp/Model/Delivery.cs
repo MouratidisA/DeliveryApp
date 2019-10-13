51d5eaddec9ec2383a018824a2272cde8b115a82
MouratidisA <argiris95@windowslive.com>
2019-10-13 17:23:59 +0300

    Adding Delivery and Delivery Person in PCL lib

---
+++ DeliveryApp/DeliveryApp/Model/Delivery.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Model
+{
+    public class Delivery
+    {
+        public string Id { get; set; }
+
+        public static async Task<List<Delivery>> GetDeliveries()
+        {
+            List<Delivery> deliveriesList = new List<Delivery>();
+
+            deliveriesList = await AzureHelper.MobileService.GetTable<Delivery>().ToListAsync();
+
+            return deliveriesList;
+        }
+    }
+}
---
+++ DeliveryApp/DeliveryApp/Model/DeliveryPerson.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Model
+{
+    public class DeliveryPerson
+    {
+        public string Id { get; set; }
+
+        public static async Task<DeliveryPerson> GetDeliveryPerson(string id)
+        {
+            DeliveryPerson person = new DeliveryPerson();
+
+            person = (await AzureHelper.MobileService.GetTable<DeliveryPerson>().Where(dp=>dp.Id==id).ToListAsync()).FirstOrDefault();
+
+            return person;
+        }
+    }
+}

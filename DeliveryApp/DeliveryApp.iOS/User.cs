c345abe8232060defddd0e2422a936be52d7b060
MouratidisA <argiris95@windowslive.com>
2019-10-13 16:37:12 +0300

    Adding User Model class for Droid and iOS

---
+++ DeliveryApp/DeliveryApp.iOS/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryApp.iOS
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

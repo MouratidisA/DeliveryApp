910a4b287245ef5e6f85234d69e36b1fab294772
MouratidisA <argiris95@windowslive.com>
2019-10-13 16:48:23 +0300

    [Droid] Adding new user in Database

--- DeliveryApp/DeliveryApp.Droid/RegisterActivity.cs
+++ DeliveryApp/DeliveryApp.Droid/RegisterActivity.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using System;
 
 namespace DeliveryApp.Droid
 {
     [Activity(Label = "RegisterActivity")]
     public class RegisterActivity : Activity
     {
-        private EditText EmailEditText, PasswordEditText, ConfirmPasswordEditText;
-        private Button RegisterUserButton;
+        private EditText _emailEditText, _passwordEditText, _confirmPasswordEditText;
+        private Button _registerUserButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -17,22 +17,34 @@ namespace DeliveryApp.Droid
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_register);
-            EmailEditText = FindViewById<EditText>(Resource.Id.RegisterEmailEditText);
-            PasswordEditText = FindViewById<EditText>(Resource.Id.RegisterPasswordEditText);
-            ConfirmPasswordEditText = FindViewById<EditText>(Resource.Id.RegisterConfirmPasswordEditText);
+            _emailEditText = FindViewById<EditText>(Resource.Id.RegisterEmailEditText);
+            _passwordEditText = FindViewById<EditText>(Resource.Id.RegisterPasswordEditText);
+            _confirmPasswordEditText = FindViewById<EditText>(Resource.Id.RegisterConfirmPasswordEditText);
 
-            RegisterUserButton = FindViewById<Button>(Resource.Id.RegisterUserButton);
+            _registerUserButton = FindViewById<Button>(Resource.Id.RegisterUserButton);
 
-            RegisterUserButton.Click += RegisterUserButton_Clicked;
+            _registerUserButton.Click += RegisterUserButton_Clicked;
 
             string email = Intent.GetStringExtra("email");
-            EmailEditText.Text = email;
+            _emailEditText.Text = email;
 
         }
 
-        private void RegisterUserButton_Clicked(object sender, EventArgs e)
+        private async void RegisterUserButton_Clicked(object sender, EventArgs e)
         {
-           
+            if (!string.IsNullOrEmpty(_passwordEditText.Text))
+            {
+                if (_passwordEditText.Text == _confirmPasswordEditText.Text)
+                {
+                    var user = new User() { Email = _emailEditText.Text, Password = _passwordEditText.Text };
+                    await MainActivity.MobileService.GetTable<User>().InsertAsync(user);
+                    Toast.MakeText(this, "Success", ToastLength.Long).Show();
+                }
+                else
+                    Toast.MakeText(this, "Passwords don't match", ToastLength.Long).Show();
+            }
+            else
+                Toast.MakeText(this, "Password cannot be empty", ToastLength.Long).Show();
         }
     }
 }

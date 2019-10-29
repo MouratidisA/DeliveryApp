1b4232ae0c4472d7588c3f6c8860085d1d3b6530
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-29 11:23:45 +0200

    [Droid] Creation of Fragments & Navigation between Fragments

--- DeliveryApp/DeliveryApp.Droid/TabsActivity.cs
+++ DeliveryApp/DeliveryApp.Droid/TabsActivity.cs
@@ -7,20 +7,52 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
 using Android.Views;
 using Android.Widget;
 
 namespace DeliveryApp.Droid
 {
     [Activity(Label = "TabsActivity")]
-    public class TabsActivity : Activity
+    public class TabsActivity : FragmentActivity
     {
+        private TabLayout _tabLayout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Tabs);
+
+            _tabLayout = FindViewById<TabLayout>(Resource.Id.mainTabLayout);
+            _tabLayout.TabSelected += TabLayout_TabSelected;
+
+            FragmentNavigate(new DeliveriesFragment());
+        }
+
+        private void TabLayout_TabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            switch (e.Tab.Position)
+            {
+                case 0:
+                    FragmentNavigate(new DeliveriesFragment());
+                    break;
+                case 1:
+                    FragmentNavigate(new DeliveredFragment());
+                    break;
+                case 2:
+                    FragmentNavigate(new ProfileFragment());
+                    break;
+            }
+        }
+
+        private void FragmentNavigate(Android.Support.V4.App.Fragment fragment)
+        {
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.contentFrame, fragment);
+            transaction.Commit();
         }
     }
 }

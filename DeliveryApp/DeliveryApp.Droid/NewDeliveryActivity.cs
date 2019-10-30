66ba0888a8383e9dc4770d56660aa596d2e3ed64
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-30 10:08:41 +0200

    [Droid] Inserting new Delivery in Database

--- DeliveryApp/DeliveryApp.Droid/NewDeliveryActivity.cs
+++ DeliveryApp/DeliveryApp.Droid/NewDeliveryActivity.cs
@@ -1,26 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using DeliveryApp.Model;
 
 namespace DeliveryApp.Droid
 {
     [Activity(Label = "NewDeliveryActivity")]
     public class NewDeliveryActivity : Activity
     {
+        private Button _saveButton;
+        private EditText _packageNameEditText;
+
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.NewDelivery);
+
+            _saveButton = FindViewById<Button>(Resource.Id.saveButton);
+            _packageNameEditText = FindViewById<EditText>(Resource.Id.packageEditText);
+
+            _saveButton.Click += SaveButton_Click;
+        }
+
+        private async void SaveButton_Click(object sender, EventArgs e)
+        {
+            Delivery delivery = new Delivery()
+            {
+                Name = _packageNameEditText.Text,
+                Status = 0
+            };
+
+            await Delivery.InsertDelivery(delivery);
         }
     }
 }
--- DeliveryApp/DeliveryApp/Model/Delivery.cs
+++ DeliveryApp/DeliveryApp/Model/Delivery.cs
@@ -6,14 +6,35 @@ namespace DeliveryApp.Model
     public class Delivery
     {
         public string Id { get; set; }
+        public string Name { get; set; }
+
+        public double OriginLatitude { get; set; }
+        public double OriginLongitude { get; set; }
+
+        public double DestinationLatitude { get; set; }
+        public double DestinationLongitude { get; set; }
+
+        /// <summary>
+        /// 0 = waiting delivery person
+        /// 1 = being delivered
+        /// 2 = delivered
+        /// </summary>
+        public int Status { get; set; }
 
         public static async Task<List<Delivery>> GetDeliveries()
         {
             List<Delivery> deliveriesList = new List<Delivery>();
 
+
             deliveriesList = await AzureHelper.MobileService.GetTable<Delivery>().ToListAsync();
 
             return deliveriesList;
+        }
+
+
+        public static async Task<bool> InsertDelivery(Delivery delivery)
+        {
+            return await AzureHelper.Insert<Delivery>(delivery);
         }
     }
 }

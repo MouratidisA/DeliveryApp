fe671f4ffb59fd13c25dd80a960311603d64f7a6
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-31 16:13:55 +0200

    [PCL] Updating Delivery & DeliveryPerson classes

--- DeliveryApp/DeliveryApp/Model/Delivery.cs
+++ DeliveryApp/DeliveryApp/Model/Delivery.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DeliveryApp.Model
@@ -21,16 +22,63 @@ namespace DeliveryApp.Model
         /// </summary>
         public int Status { get; set; }
 
+
+        public string DeliveryPersonId { get; set; }
+
+        public static async Task<bool> MarkAsPickedUp(Delivery delivery,string deliveryPersonId)
+        {
+            try
+            {
+                delivery.Status = 1;
+                delivery.DeliveryPersonId = deliveryPersonId;
+                await AzureHelper.MobileService.GetTable<Delivery>().UpdateAsync(delivery);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static async Task<bool> MarkAsDelivered(Delivery delivery)
+        {
+            try
+            {
+                delivery.Status = 2;                
+                await AzureHelper.MobileService.GetTable<Delivery>().UpdateAsync(delivery);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
         public static async Task<List<Delivery>> GetDeliveries()
         {
             List<Delivery> deliveriesList = new List<Delivery>();
             deliveriesList = await AzureHelper.MobileService.GetTable<Delivery>().Where(d => d.Status != 2).ToListAsync();
             return deliveriesList;
         }
+
         public static async Task<List<Delivery>> GetDelivered()
         {
             List<Delivery> deliveredList = new List<Delivery>();
             deliveredList = await AzureHelper.MobileService.GetTable<Delivery>().Where(d => d.Status == 2).ToListAsync();
+            return deliveredList;
+        }
+
+        public static async Task<List<Delivery>> GetBeingDelivered(string id)
+        {
+            List<Delivery> deliveredList = new List<Delivery>();
+            deliveredList = await AzureHelper.MobileService.GetTable<Delivery>().Where(d => d.Status == 1 && d.DeliveryPersonId == id).ToListAsync();
+            return deliveredList;
+        }
+
+        public static async Task<List<Delivery>> GetWaiting()
+        {
+            List<Delivery> deliveredList = new List<Delivery>();
+            deliveredList = await AzureHelper.MobileService.GetTable<Delivery>().Where(d => d.Status == 0).ToListAsync();
             return deliveredList;
         }
 
--- DeliveryApp/DeliveryApp/Model/DeliveryPerson.cs
+++ DeliveryApp/DeliveryApp/Model/DeliveryPerson.cs
@@ -6,6 +6,45 @@ namespace DeliveryApp.Model
     public class DeliveryPerson
     {
         public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public static async Task<bool> Login(string email, string password)
+        {
+            bool result = false;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return result;
+            }
+
+            var deliveryPerson = (await AzureHelper.MobileService.GetTable<DeliveryPerson>().Where(u => u.Email == email).ToListAsync()).FirstOrDefault();
+            if (deliveryPerson?.Password == password)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+
+        public static async Task<bool> Register(string email, string password, string confirmPassword)
+        {
+            bool result = false;
+
+            if (!string.IsNullOrEmpty(password) && (password == confirmPassword))
+            {
+                var deliveryPerson = new DeliveryPerson() { Email = email, Password = password };
+
+                await AzureHelper.Insert(deliveryPerson);
+
+                result = true;
+            }
+
+            return result;
+        }
+
 
         public static async Task<DeliveryPerson> GetDeliveryPerson(string id)
         {

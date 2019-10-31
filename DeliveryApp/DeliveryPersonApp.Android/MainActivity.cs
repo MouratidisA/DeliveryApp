26209446c124ab7fa6cc8b9707192e0cf29d8f93
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-31 14:48:22 +0200

    [Droid] PCL added, Login and Register functionalities added

--- DeliveryApp/DeliveryPersonApp.Android/MainActivity.cs
+++ DeliveryApp/DeliveryPersonApp.Android/MainActivity.cs
@@ -1,17 +1,64 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Widget;
+using System;
 
 namespace DeliveryPersonApp.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private EditText _emailEditText, _passwordEditText;
+        private Button _signinButton, _registerButton;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            _emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            _passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            _signinButton = FindViewById<Button>(Resource.Id.signinButton);
+            _registerButton = FindViewById<Button>(Resource.Id.registerButton);
+
+            _signinButton.Click += SignInButton_Click;
+            _registerButton.Click += RegisterButton_Click;
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));            
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            var email = _emailEditText.Text;
+            var password = _passwordEditText.Text;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                Toast.MakeText(this, "Email and password cannot be empty", ToastLength.Long).Show();
+            }
+            else
+            {
+                //var result = await User.Login(email, password); TODO uncomment when Azure Service is available 
+                var result = true;
+
+                if (result)
+                {
+                    Toast.MakeText(this, "Login successful", ToastLength.Long).Show();
+                    StartActivity(typeof(TabsActivity));
+                    Finish();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Incorrect password", ToastLength.Long).Show();
+                }
+            }
+         
         }
     }
 }
--- DeliveryApp/DeliveryPersonApp.Android/RegisterActivity.cs
+++ DeliveryApp/DeliveryPersonApp.Android/RegisterActivity.cs
@@ -1,26 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using DeliveryApp.Model;
+using System;
 
 namespace DeliveryPersonApp.Android
 {
     [Activity(Label = "RegisterActivity")]
     public class RegisterActivity : Activity
     {
+        private EditText _emailEditText, _passwordEditText, _confirmPasswordEditText;
+        private Button _registerButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Register);
+            
+            _emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            _passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            _confirmPasswordEditText = FindViewById<EditText>(Resource.Id.registerConfirmPasswordEditText);            
+            _registerButton = FindViewById<Button>(Resource.Id.registerButton);
+            
+            _registerButton.Click += RegisterButton_Click;
+        }
+
+        private async void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_passwordEditText.Text))
+            {
+                if (_passwordEditText.Text == _confirmPasswordEditText.Text)
+                {
+                    if (await User.Register(_emailEditText.Text, _passwordEditText.Text, _confirmPasswordEditText.Text))
+                        Toast.MakeText(this, "Success", ToastLength.Long).Show();
+                    else
+                        Toast.MakeText(this, "Try again", ToastLength.Long).Show();
+                }
+                else
+                    Toast.MakeText(this, "Passwords don't match", ToastLength.Long).Show();
+            }
+            else
+                Toast.MakeText(this, "Password cannot be empty", ToastLength.Long).Show();
         }
     }
 }

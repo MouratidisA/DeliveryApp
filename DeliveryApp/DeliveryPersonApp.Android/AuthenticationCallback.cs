f3c884cb9050d84fb655153c31285cba93c3511a
Mouratidis Anargyros <argiris95@windowslive.com>
2019-11-01 11:05:13 +0200

    [Droid] DeliveryPerson Biometric Authentication

---
+++ DeliveryApp/DeliveryPersonApp.Android/AuthenticationCallback.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Support.V4.Hardware.Fingerprint;
+using Android.Widget;
+
+namespace DeliveryPersonApp.Android
+{
+    internal class AuthenticationCallback : FingerprintManagerCompat.AuthenticationCallback
+    {
+        private Activity _activity;
+        private string _userId;
+
+        public AuthenticationCallback(Activity activity, string userId)
+        {
+            _activity = activity;
+            _userId = userId;
+        }
+
+        public override void OnAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result)
+        {
+            base.OnAuthenticationSucceeded(result);
+
+            Toast.MakeText(_activity, "Login successful", ToastLength.Long).Show();
+            Intent intent = new Intent(_activity, typeof(TabsActivity));
+            intent.PutExtra("userId", _userId);
+            _activity.StartActivity(intent);
+        }
+
+
+        public override void OnAuthenticationFailed()
+        {
+            base.OnAuthenticationFailed();
+
+            Toast.MakeText(_activity, "Failure", ToastLength.Long).Show();
+        }
+    }
+}
--- DeliveryApp/DeliveryPersonApp.Android/MainActivity.cs
+++ DeliveryApp/DeliveryPersonApp.Android/MainActivity.cs
@@ -1,10 +1,14 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
+using Android.Content;
 using Android.OS;
+using Android.Preferences;
+using Android.Support.V4.Content;
+using Android.Support.V4.Hardware.Fingerprint;
 using Android.Support.V7.App;
 using Android.Widget;
-using System;
-using Android.Content;
 using DeliveryApp.Model;
+using System;
 
 namespace DeliveryPersonApp.Android
 {
@@ -13,12 +17,22 @@ namespace DeliveryPersonApp.Android
     {
         private EditText _emailEditText, _passwordEditText;
         private Button _signinButton, _registerButton;
+        private FingerprintManagerCompat _fingerprintManagerCompat;
+        private global::Android.Support.V4.OS.CancellationSignal _cancelation;
+        private ISharedPreferences _sharedPreferences;
+        private string _userId;
 
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
+
+            _fingerprintManagerCompat = FingerprintManagerCompat.From(this);
+            _cancelation = new global::Android.Support.V4.OS.CancellationSignal();
+            _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(this);
+            //TODO rest shared Preferences: Application.Context.GetSharedPreferences("sampleSharedPreferencesName",FileCreationMode.Private);
+
+
             SetContentView(Resource.Layout.Main);
 
             _emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
@@ -32,37 +46,84 @@ namespace DeliveryPersonApp.Android
 
         private void RegisterButton_Click(object sender, EventArgs e)
         {
-            StartActivity(typeof(RegisterActivity));            
+            StartActivity(typeof(RegisterActivity));
         }
 
         private async void SignInButton_Click(object sender, EventArgs e)
         {
+            var canUseFingerprint = CanUseFingerprint();
 
-            var email = _emailEditText.Text;
-            var password = _passwordEditText.Text;
-
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            if (canUseFingerprint)
             {
-                Toast.MakeText(this, "Email and password cannot be empty", ToastLength.Long).Show();
+                LogUserIn();
             }
             else
             {
-                var userId = await DeliveryPerson.Login(email, password);
-                
-                if (!string.IsNullOrEmpty(userId))
+                var email = _emailEditText.Text;
+                var password = _passwordEditText.Text;
+
+                if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
                 {
-                    Toast.MakeText(this, "Login successful", ToastLength.Long).Show();
-                    Intent intent = new Intent(this,typeof(TabsActivity));
-                    intent.PutExtra("userId",userId);                    
-                    StartActivity(intent);
-                    Finish();
+                    Toast.MakeText(this, "Email and password cannot be empty", ToastLength.Long).Show();
                 }
                 else
                 {
-                    Toast.MakeText(this, "Incorrect password", ToastLength.Long).Show();
+                    _userId = await DeliveryPerson.Login(email, password);
+
+                    if (!string.IsNullOrEmpty(_userId))
+                    {
+
+                        var preferencesEditor = _sharedPreferences.Edit();
+                        preferencesEditor.PutString("userId", _userId);
+                        preferencesEditor.Apply();
+
+
+                        Toast.MakeText(this, "Login successful", ToastLength.Long).Show();
+                        Intent intent = new Intent(this, typeof(TabsActivity));
+                        intent.PutExtra("userId", _userId);
+                        StartActivity(intent);
+                        Finish();
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Incorrect password", ToastLength.Long).Show();
+                    }
                 }
             }
-         
+
+        }
+
+        private bool CanUseFingerprint()
+        {
+            _userId = _sharedPreferences.GetString("userId", String.Empty);
+
+            if (!String.IsNullOrEmpty(_userId))
+            {
+
+                if (_fingerprintManagerCompat.IsHardwareDetected)
+                {
+                    if (_fingerprintManagerCompat.HasEnrolledFingerprints)
+                    {
+                        var permissionResult = ContextCompat.CheckSelfPermission(this, Manifest.Permission.UseFingerprint);
+                        if (permissionResult == global::Android.Content.PM.Permission.Granted)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void LogUserIn()
+        {
+            _cancelation = new global::Android.Support.V4.OS.CancellationSignal();
+            FingerprintManagerCompat.AuthenticationCallback authenticationCallback = new AuthenticationCallback(this, _userId);
+            Toast.MakeText(this, "Place fingerprint on sensor", ToastLength.Long).Show();
+            _fingerprintManagerCompat.Authenticate(null, 0, _cancelation, authenticationCallback, null);
         }
     }
+
+
 }
